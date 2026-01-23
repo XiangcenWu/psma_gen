e460913fdae88ed58f5c25ff5412df66fcb178c8
XiangcenWu <wxiangcen@gmail.com>
2026-01-23 16:31:04 +0800

    add random stuff

--- data_process/Inference_TS.py
+++ data_process/Inference_TS.py
@@ -30,3 +30,26 @@ def get_ct_bodymask(patients_dir):
         inference_ts(fdgCT_dir, fdgCT_mask_dir)
         inference_ts(psmaCT_dir, psmaCT_mask_dir)
 
+
+
+def get_ct_totalmask(patients_dir):
+    
+    fdgCT_name = 'fdgCT_series2.nii.gz'
+    psmaCT_name = 'psmaCT_series2.nii.gz'
+    
+    fdgCT_mask_name = 'fdgCT_total_mask.nii.gz'
+    psmaCT_mask_name = 'psmaCT_total_mask.nii.gz'
+    
+    
+    for patient_dir in tqdm(os.listdir(patients_dir), desc='Get Ct full Mask', unit='case'):
+        print(patient_dir)
+
+        fdgCT_dir = os.path.join(patients_dir, patient_dir, fdgCT_name)
+        psmaCT_dir = os.path.join(patients_dir, patient_dir, psmaCT_name)
+    
+        fdgCT_mask_dir = os.path.join(patients_dir, patient_dir, fdgCT_mask_name)
+        psmaCT_mask_dir = os.path.join(patients_dir, patient_dir, psmaCT_mask_name)
+
+
+        inference_ts(fdgCT_dir, fdgCT_mask_dir, task='total')
+        inference_ts(psmaCT_dir, psmaCT_mask_dir, task='total')
--- data_process/data_parse.py
+++ data_process/data_parse.py
@@ -1,43 +1,51 @@
 import argparse
 # personal imports
 from ProcessZip import process_zip_files
-from Inference_TS import get_ct_bodymask
+from Inference_TS import get_ct_bodymask, get_ct_totalmask
 from Dicom2Nii import separate_dicom_modalities
 from cropAintensiry import crop_and_intensity
 import os
 
-parser = \
-    argparse.ArgumentParser(description="Process medical archives, run inference, and convert to H5.")
-parser.add_argument(
-    "--pressed_dir",
-    type=str,
-    required=True,
-    help="Path to the directory containing ZIP/RAR archive files."
-)
-parser.add_argument(
-    "--patient_dir",
-    type=str,
-    required=True,
-    help="Path to the output directory where processed patient data will be saved."
-)
-parser.add_argument(
-    "--ts_dir",
-    type=str,
-    required=True,
-    help="Path to the ts model dir."
-)
 
 
 
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = \
+        argparse.ArgumentParser(description="Process medical archives, run inference, and convert to H5.")
+    parser.add_argument(
+        "--pressed_dir",
+        type=str,
+        required=True,
+        help="Path to the directory containing ZIP/RAR archive files."
+    )
+    parser.add_argument(
+        "--patient_dir",
+        type=str,
+        required=True,
+        help="Path to the output directory where processed patient data will be saved."
+    )
+    parser.add_argument(
+        "--ts_dir",
+        type=str,
+        required=True,
+        help="Path to the ts model dir."
+    )
 
 
 
-os.environ["TOTALSEG_WEIGHTS_PATH"] = args.ts_dir
+    args = parser.parse_args()
 
-# process all zip/rar files and saved to one place
-process_zip_files(args.pressed_dir, args.patient_dir)
-# inference using totalsegmentator
-get_ct_bodymask(args.patient_dir)
-# crop and intensity range then convert to h5
-crop_and_intensity(args.patient_dir)
+
+
+    os.environ["TOTALSEG_WEIGHTS_PATH"] = args.ts_dir
+
+    # process all zip/rar files and saved to one place
+    # process_zip_files(args.pressed_dir, args.patient_dir)
+    # inference using totalsegmentator
+    # get_ct_bodymask(args.patient_dir)
+    # crop and intensity range then convert to h5
+    # crop_and_intensity(args.patient_dir)
+
+
+    # get total mask
+    get_ct_totalmask(args.patient_dir)
