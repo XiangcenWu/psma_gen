f4538b3d7b7bb745f5a6845109ec8beaf664dbe3
XiangcenWu <93538241+XiangcenWu@users.noreply.github.com>
2026-02-24 14:58:27 +0800

    add generate_warp file

---
+++ generate_warp.sh
@@ -0,0 +1,86 @@
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/baseline_l4000_k10.ptm \
+--result_dir ~/baseline_l4000_k10 
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/baseline_l4500_k10.ptm \
+--result_dir ~/baseline_l4500_k10 
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/baseline_l5000_k10.ptm \
+--result_dir ~/baseline_l5000_k10 
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/baseline_l5500_k10.ptm \
+--result_dir ~/baseline_l5500_k10 
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/baseline_l6000_k10.ptm \
+--result_dir ~/baseline_l6000_k10 
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/baseline_l6500_k10.ptm \
+--result_dir ~/baseline_l6500_k10 
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/baseline_l7000_k10.ptm \
+--result_dir ~/baseline_l7000_k10 
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l4000_k10_mar3000_gam1.0.ptm \
+--result_dir ~/ctsmoothness_l4000_k10_mar3000_gam1.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l4000_k10_mar3000_gam2.0.ptm \
+--result_dir ~/ctsmoothness_l4000_k10_mar3000_gam2.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l4500_k10_mar3000_gam1.0.ptm \
+--result_dir ~/ctsmoothness_l4500_k10_mar3000_gam1.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l4500_k10_mar3000_gam2.0.ptm \
+--result_dir ~/ctsmoothness_l4500_k10_mar3000_gam2.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l5000_k10_mar3000_gam1.0.ptm \
+--result_dir ~/ctsmoothness_l5000_k10_mar3000_gam1.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l5000_k10_mar3000_gam2.0.ptm \
+--result_dir ~/ctsmoothness_l5000_k10_mar3000_gam2.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l5500_k10_mar3000_gam1.0.ptm \
+--result_dir ~/ctsmoothness_l5500_k10_mar3000_gam1.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l5500_k10_mar3000_gam2.0.ptm \
+--result_dir ~/ctsmoothness_l5500_k10_mar3000_gam2.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l6000_k10_mar3000_gam1.0.ptm \
+--result_dir ~/ctsmoothness_l6000_k10_mar3000_gam1.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l6000_k10_mar3000_gam2.0.ptm \
+--result_dir ~/ctsmoothness_l6000_k10_mar3000_gam2.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l6500_k10_mar3000_gam1.0.ptm \
+--result_dir ~/ctsmoothness_l6500_k10_mar3000_gam1.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l6500_k10_mar3000_gam2.0.ptm \
+--result_dir ~/ctsmoothness_l6500_k10_mar3000_gam2.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l7000_k10_mar3000_gam1.0.ptm \
+--result_dir ~/ctsmoothness_l7000_k10_mar3000_gam1.0
+
+python Registration/generate_warp.py \
+--weights_path /data1/xiangcen/models/registration_v2/ctsmoothness_l7000_k10_mar3000_gam2.0.ptm \
+--result_dir ~/ctsmoothness_l7000_k10_mar3000_gam2.0
+
+
+
