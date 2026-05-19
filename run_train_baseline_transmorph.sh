3ebdfbcff321d8145020dcef6f25c41f0a8dac6b
XiangcenWu <wxiangcen@gmail.com>
2026-05-20 01:49:04 +0800

    add baseline slurm files

---
+++ run_train_baseline_transmorph.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=transmorph
+#SBATCH --partition=gpu
+#SBATCH --nodelist=gpu02
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --output=log_transmorph_%j.out
+#SBATCH --error=log_transmorph_%j.err
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate gen
+cd ~/projects/psma_gen
+
+python Registration/train_baseline.py \
+    --baseline_model transmorph \
+    --smoothness 4500 \
+    --epochs 350 \
+    --num_masks 10 \
+    --lr 1e-5 \
+    --device cuda:0
---
+++ run_train_baseline_voxelmorph.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=voxelmorph
+#SBATCH --partition=gpu
+#SBATCH --nodelist=gpu02
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --output=log_voxelmorph_%j.out
+#SBATCH --error=log_voxelmorph_%j.err
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate gen
+cd ~/projects/psma_gen
+
+python Registration/train_baseline.py \
+    --baseline_model voxelmorph \
+    --smoothness 4500 \
+    --epochs 350 \
+    --num_masks 10 \
+    --lr 1e-5 \
+    --device cuda:0
